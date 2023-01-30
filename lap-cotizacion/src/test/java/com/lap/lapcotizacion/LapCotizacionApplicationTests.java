103c4dea9daf307c762897a0dbf08cbf8a8e9ddc
Cesarpa Code <cesar.1023@hotmail.com>
2023-01-29 19:39:21 -0500

    LAP app created lombok jpa

--- lap-cotizacion/src/test/java/com/lap/lapcotizacion/LapCotizacionApplicationTests.java
+++ lap-cotizacion/src/test/java/com/lap/lapcotizacion/LapCotizacionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cotizacion.demo;
+package com.lap.lapcotizacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class LapCotizacionApplicationTests {
 
 	@Test
 	void contextLoads() {
